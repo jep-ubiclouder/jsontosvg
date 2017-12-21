183fde7a120ffc8e1e3be01306bc8c9dfe5e4d1f
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-12-21 17:07:40 +0100

    go

--- ubJSONToSVG.js
+++ ubJSONToSVG.js
@@ -24,16 +24,31 @@ records.push({  'dDeb':new Date(2017,11, 27, 11, 0, 30, 0),
 function getBornes(tRecords){
 	var dMin = new Date(2077,0,1);
 	var dMax = new Date(1971,0,1);
+	
+	var resultat = {};
 	for(k in tRecords){
 		var r = tRecords[k];
 		console.log(r);
-		if(r.dDeb < dMin){ dMin=r.dDeb ;}
-		if(dMax < r.dFin){ dMax=r.dFin ;}
-		console.log(r.dFin/MILLISECONDSPERHOURS - r.dDeb/MILLISECONDSPERHOURS);
+		if(r.dDeb > r.dFin){ // sanity check
+			var tmp = new Date();
+			tmp = r.dDeb;
+			r.dDeb = r.dFin;
+			r.dFin = tmp;
+		}
+		
+		if(r.dDeb < dMin){ 
+			dMin=r.dDeb ;
+			}
+		if(dMax < r.dFin){ 
+			dMax=r.dFin ;
+			}
+		r.duration = (r.dFin/MILLISECONDSPERHOURS - r.dDeb/MILLISECONDSPERHOURS);
 	}
-	console.log(dMax);
-	console.log(dMin);
-	console.log(dMax/MILLISECONDSPERHOURS - dMin/MILLISECONDSPERHOURS);
+	resultat.dMax = dMax;
+	resultat.dMin = dMin;
+	resultat.dureeMax=(dMax/MILLISECONDSPERHOURS - dMin/MILLISECONDSPERHOURS);
+	return resultat;
 }
 
-getBornes(records);
+var r = getBornes(records);
+console.log(r);
