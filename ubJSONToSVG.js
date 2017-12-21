12e1e728652588167e49984075ba89ef726691d5
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-12-21 16:59:45 +0100

    go

--- ubJSONToSVG.js
+++ ubJSONToSVG.js
@@ -14,8 +14,8 @@ records.push({  'dDeb':new Date(2017,11, 24, 11, 0, 30, 0),
 				'tLabel':'LABEL A',
 				'duration':0});
 
-records.push({  'Ddeb':new Date(2017,11, 20, 11, 0, 30, 0),
-				'Dfin':new Date(2017,11, 30, 11, 0, 30, 0),
+records.push({  'dDeb':new Date(2017,11, 20, 11, 0, 30, 0),
+				'dFin':new Date(2017,11, 30, 11, 0, 30, 0),
 				'tId':'B',
 				'tLabel':'LABEL B',
 				'duration':0});
@@ -27,8 +27,8 @@ function getBornes(tRecords){
 	for(k in tRecords){
 		var r = tRecords[k];
 		console.log(r);
-		if(r.dDeb<dMin){ dMin=r.dDeb ;}
-		if(dMax<r.dFin){ dMax=r.dFin ;}
+		if(r.dDeb < dMin){ dMin=r.dDeb ;}
+		if(dMax < r.dFin){ dMax=r.dFin ;}
 		console.log(r.dFin/MILLISECONDSPERHOURS - r.dDeb/MILLISECONDSPERHOURS);
 	}
 	console.log(dMax);
