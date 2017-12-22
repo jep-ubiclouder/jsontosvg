2d15aa1290f0b97fe74fa521c3a2eb7fa44bb4a3
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-12-22 10:24:13 +0100

    oo

--- ubJSONToSVG.js
+++ ubJSONToSVG.js
@@ -55,46 +55,90 @@ function getBornes(tRecords){
 }
 
 
-function json2xml(json) {
-    var a = JSON.parse(json)
-    var c = document.createElement("root");
-    var t = function (v) {
-        return {}.toString.call(v).split(' ')[1].slice(0, -1).toLowerCase();
-    };
-    var f = function (f, c, a, s) {
-        c.setAttribute("type", t(a));
-        if (t(a) != "array" && t(a) != "object") {
-            if (t(a) != "null") {
-                c.appendChild(document.createTextNode(a));
-            }
-        } else {
-            for (var k in a) {
-                var v = a[k];
-                if (k == "__type" && t(a) == "object") {
-                    c.setAttribute("__type", v);
-                } else {
-                    if (t(v) == "object") {
-                        var ch = c.appendChild(document.createElementNS(null, s ? "item" : k));
-                        f(f, ch, v);
-                    } else if (t(v) == "array") {
-                        var ch = c.appendChild(document.createElementNS(null, s ? "item" : k));
-                        f(f, ch, v, true);
-                    } else {
-                        var va = document.createElementNS(null, s ? "item" : k);
-                        if (t(v) != "null") {
-                            va.appendChild(document.createTextNode(v));
-                        }
-                        var ch = c.appendChild(va);
-                        ch.setAttribute("type", t(v));
-                    }
-                }
-            }
-        }
-    };
-    f(f, c, a, t(a) == "array");
-    return c.outerHTML;
-}
-
+function json2xml(o) {
+	if (typeof o == 'object' && o.constructor == Object && len(o) == 1) {
+		for (var a in o) {
+			return toXML(a, o[a]);
+		}
+	} else {
+ 
+	}
+	function len(o) {
+		var n = 0;
+		for (var a in o) {
+			n++;
+		}
+		return n;
+	}
+	function toXML(tag, o) {
+		var doc = '<' + tag;
+		if (typeof o === 'undefined' || o === null) {
+			doc += '/>';
+			return doc;
+		}
+		if (typeof o !== 'object') {
+			doc += '>' + safeXMLValue(o) + '</' + tag + '>';
+			return doc;
+		}
+		if (o.constructor == Object) {
+			for (var a in o) {
+				if (a.charAt(0) == '@') {
+					if (typeof o[a] !== 'object') {
+						doc += ' ' + a.substring(1) + '="' + o[a] + '"';
+						delete o[a];
+					} else {
+						throw new Error((typeof o[a])
+								+ ' being attribute is not supported.');
+					}
+				}
+			}
+			if (len(o) === 0) {
+				doc += '/>';
+				return doc;
+			} else {
+				doc += '>';
+			}
+			if (typeof o['#text'] !== 'undefined') {
+				if (typeof o['#text'] !== 'object') {
+					doc += o['#text'] + '</' + tag + '>';
+					return doc;
+				} else {
+					throw new Error((typeof o['#text'])
+							+ ' being #text is not supported.');
+				}
+			} else {
+				for (var b in o) {
+					if (o[b].constructor == Array) {
+						for (var i = 0; i < o[b].length; i++) {
+							if (typeof o[b][i] !== 'object'
+									|| o[b][i].constructor == Object) {
+								doc += toXML(b, o[b][i]);
+							} else {
+								throw new Error((typeof o[b][i])
+										+ ' is not supported.');
+							}
+						}
+					} else if (o[b].constructor == Object
+							|| typeof o[b] !== 'object') {
+						doc += toXML(b, o[b]);
+					} else {
+						throw new Error((typeof o[b]) + ' is not supported.');
+					}
+				}
+			}
+			doc += '</' + tag + '>'
+			return doc;
+		}
+	}
+	function safeXMLValue(value) {
+		var s = value.toString();
+		s = s.replace('/\&/g', '&amp;');
+		s = s.replace('/\"/g', '&quot;');
+		s = s.replace('/</g', '&lt;');
+		s = s.replace('/>/g', '&gt;');
+		return s;
+	}
+};
 var meta = getBornes(records);
 console.log(meta);
 console.log(records);
