3dda8ef92261fecda604b91aceab98258a97bbba
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-12-22 10:15:44 +0100

    jq

--- ubJSONToSVG.js
+++ ubJSONToSVG.js
@@ -55,27 +55,44 @@ function getBornes(tRecords){
 }
 
 
-function objectToXml(obj) {
-    var xml = '';
-
-    for (var prop in obj) {
-        if (!obj.hasOwnProperty(prop)) {
-            continue;
+function json2xml(json) {
+    var a = JSON.parse(json)
+    var c = document.createElement("root");
+    var t = function (v) {
+        return {}.toString.call(v).split(' ')[1].slice(0, -1).toLowerCase();
+    };
+    var f = function (f, c, a, s) {
+        c.setAttribute("type", t(a));
+        if (t(a) != "array" && t(a) != "object") {
+            if (t(a) != "null") {
+                c.appendChild(document.createTextNode(a));
+            }
+        } else {
+            for (var k in a) {
+                var v = a[k];
+                if (k == "__type" && t(a) == "object") {
+                    c.setAttribute("__type", v);
+                } else {
+                    if (t(v) == "object") {
+                        var ch = c.appendChild(document.createElementNS(null, s ? "item" : k));
+                        f(f, ch, v);
+                    } else if (t(v) == "array") {
+                        var ch = c.appendChild(document.createElementNS(null, s ? "item" : k));
+                        f(f, ch, v, true);
+                    } else {
+                        var va = document.createElementNS(null, s ? "item" : k);
+                        if (t(v) != "null") {
+                            va.appendChild(document.createTextNode(v));
+                        }
+                        var ch = c.appendChild(va);
+                        ch.setAttribute("type", t(v));
+                    }
+                }
+            }
         }
-
-        if (obj[prop] == undefined)
-            continue;
-
-        xml += "<" + prop + ">";
-        if (typeof obj[prop] == "object")
-            xml += objectToXml(new Object(obj[prop]));
-        else
-            xml += obj[prop];
-
-        xml += "</" + prop + ">";
-    }
-
-    return xml;
+    };
+    f(f, c, a, t(a) == "array");
+    return c.outerHTML;
 }
 
 var meta = getBornes(records);
@@ -91,5 +108,5 @@ for(k in records){
 	r.pxWidth = Math.floor(r.duration * PIXELPERHOUR);
 }
 console.log(records);
-var xml =  objectToXml(records[0]);
+var xml =  json2xml(records[0]);
 console.log(xml);
