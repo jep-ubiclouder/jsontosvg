d3d3736de84bac4bc78ed0ff41c115db9e2306d8
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-12-22 10:54:28 +0100

    df

--- ubJSONToSVG.js
+++ ubJSONToSVG.js
@@ -61,6 +61,7 @@ function parseJson(o){
 		console.log('clef dans o',t);
 		console.log('valeur clef dans o',o[t])
 		console.log('type of valeur',typeof o[t]);
+		console.log('longueur de valeur',typeof o[t]);
 	}
 	
 }
