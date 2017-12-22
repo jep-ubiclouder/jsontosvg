bf8a55ec74470f108df96bf8e9fabcedbf7f0ac7
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-12-22 10:32:44 +0100

    dfq

--- ubJSONToSVG.js
+++ ubJSONToSVG.js
@@ -57,7 +57,7 @@ function getBornes(tRecords){
 
 function json2xml(o) {
 	console.log('json to xml o',len(o) );
-	if (typeof o == 'object' && o.constructor == Object && len(o) == 1) {
+	if (typeof o == 'object' && o.constructor == Object) { //  && len(o) == 1
 		for (var a in o) {
 			console.log('a',a);
 			return toXML(a, o[a]);
